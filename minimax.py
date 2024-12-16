f26a0fe20784cdc66b03f7d09284e0479a7e4153
Aidan Work <aidankatz1835@gmail.com>
2024-12-16 16:51:38 -0500

    added TreeNode class to minimax.py

--- minimax.py
+++ minimax.py
@@ -1,3 +1,8 @@
+# TODO: Store a hint_move or hint_state to offer hints to the user (their optimal move)
+class TreeNode:
+    def __init__(self):
+        pass
+
 # Used to generate the minimax tree for the game
 def generate_valid_states(current_state):
     # Generate all possible valid states from the current state, return a list of states
